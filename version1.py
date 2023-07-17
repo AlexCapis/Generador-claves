9198593a8d0418badbf32e1e10e44221f59c779f
AlexCapis <alexmarza17@gmail.com>
2023-07-17 23:03:17 +0200

    se añade el fichero generador_contraseña y el fichero de version1.py

---
+++ version1.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+
+
+def mostrar_recomendaciones():
+    titulo = "¡Recomendaciones para crear contraseñas seguras!"
+    linea = "*" * 50
+
+    print(linea)
+    print(titulo.upper().center(50))
+    print(linea)
+    print("1. Sé creativo: Combina letras mayúsculas y minúsculas. (Ejemplo: AbCdEfG)")
+    print("2. Añade números y evita información personal obvia. (Ejemplo: P4$$w0rd)")
+    print("3. Utiliza caracteres especiales para mayor seguridad. (Ejemplo: MyP@$$w0rd!)")
+    print("4. Evita secuencias comunes o patrones predecibles. (Ejemplo: QWERTY123)")
+    print("5. Mantén la privacidad, evita datos personales. (Ejemplo: JohnDoe1985)")
+    print("6. Elige contraseñas con longitud suficiente. (Ejemplo: MyLongPassword123)")
+    print("7. No reutilices contraseñas en diferentes cuentas. (Ejemplo: !Password123 para cada cuenta)")
+    print("8. Actualiza tus contraseñas periódicamente. (Ejemplo: UpdatePassword2023!)")
+    print("9. Considera el uso de un gestor de contraseñas. (Ejemplo: LastPass, 1Password)")
+    print(linea)
+    print()
+
+mostrar_recomendaciones()
+
+
+def generar_contrasena(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
+    resultado = "Tu contraseña contiene {} caracteres y es la siguiente: {}".format(longitud, contrasena)
+    return resultado
+
+generar_contrasena(int(input("Introduce el número de caracteres que quieres que contenga tu contraseña ")))
+
